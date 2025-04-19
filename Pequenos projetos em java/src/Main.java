02c1e34169a93e23764eb3c2249d632947b25e28
Pedro Moura <pmouramiguelmoura@gmail.com>
2025-04-18 22:35:31 -0300

    pequenos projetos em java

---
+++ Pequenos projetos em java/src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int vel = 70;
+        if(vel >= 60){
+            System.out.println("multa por excesso de velocidade");
+        } else {
+            System.out.println("tudo certo!!");
+        }
+        System.out.println("Use cinto de segurança!");
+    }
+
+}
---
+++ Pequenos projetos em java/src/idade.java
@@ -0,0 +1,13 @@
+import java.time.Year;
+
+public class idade {
+
+    public static void main(String[] args) {
+        int anoatual = Year.now().getValue();
+        int nasceu = 2007;
+                int idade = anoatual - nasceu;
+            System.out.println(String.format("Você tem %d ou faz %d esse ano.", idade, idade));
+
+    }
+
+}
